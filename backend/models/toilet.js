136cfbc87f011a4bbe65fe5c67125b34cde2665e
Amber <gynteee@yahoo.co.uk>
2023-03-28 17:16:31 +0100

    Amended formatting

--- backend/models/toilet.js
+++ backend/models/toilet.js
@@ -1,14 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 mongoose.set("useFindAndModify", false);
 
-const ToiletSchema = new mongoose.Schema({
-  name: { type: string, unique: true },
-  accessible: { type: boolean },
-  babyChanging: { type: boolean };
-  price: { type: double },
-  addedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User", immutable: true },
-}, { timestamps: true })
+const ToiletSchema = new mongoose.Schema(
+  {
+    name: { type: String, unique: true },
+    accessible: { type: Boolean },
+    babyChanging: { type: Boolean },
+    price: { type: double },
+    addedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      immutable: true,
+    },
+  },
+  { timestamps: true }
+);
 
 const Toilet = mongoose.model("Toilet", ToiletSchema);
 
