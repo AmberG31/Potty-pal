466b48429856b02cc480b039962b188355a0d270
Chris Leech <chrisleech@monzo.com>
2023-03-29 11:18:57 +0100

    Broken: Trying to fix user validation

--- backend/controllers/toilets.js
+++ backend/controllers/toilets.js
@@ -3,6 +3,7 @@ const Address = require("../models/address");
 const User = require("../models/user");
 const Review = require("../models/review");
 const generateToken = require("../models/tokenGenerator");
+const tokenChecker = require("../middleware/tokenChecker");
 
 const getAllToilets = async (req, res) => {
   try {
@@ -31,21 +32,21 @@ const getAllToilets = async (req, res) => {
 
 const addNewToilet = async (req, res) => {
   try {
-    const { name, accessible, babyChanging, price, address, addedBy } =
-      req.body;
+    const { name, accessible, babyChanging, price, address } = req.body;
     const toilet = await new Toilet({
       name: name,
       accessible: accessible,
       babyChanging: babyChanging,
       price: price,
+      addedBy: req.userId,
       address: address,
-      addedBy: addedBy,
     });
+    // const toilet = await new Toilet(req.body);
     await toilet.save();
 
     // Generate a new token
     const token = await generateToken(req.userId);
-    res.status(201).json({ message: "OK", token });
+    res.status(201).json({ toilet, token });
   } catch (error) {
     res.status(500).json({ error_message: error.message });
   }
