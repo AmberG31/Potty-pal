4ef74a6a797cbedd6ab7532350d9c2a7225b229e
Chris Leech <chrisleech@monzo.com>
2023-03-29 11:19:20 +0100

    Creation of ToiletList

--- frontend/src/components/toiletList/ToiletList.js
+++ frontend/src/components/toiletList/ToiletList.js
@@ -1,35 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, {useState} from "react";
 import Toilet from "../toilet/Toilet";
 
 const ToiletList = () => {
   const [toilets, setToilets] = useState([]);
 
   const getToilets = async () => {
-    const response = await fetch("/toilets");
 
-    if (response.status !== 200) {
-      throw new Error("Cannot fetch the data");
-    } else {
-      const data = await response.json();
-      setToilets(data.toilets);
-    }
-  };
-
-  useEffect(() => {
-    getToilets();
-  }, []);
+  }
 
   return (
     <>
-      {/* Create the body of the Toilet List which will return a Toilet Component */}
-      <div>
-        <h1>Hello</h1>
-        {toilets.map((toilet) => (
-          <Toilet toilet={toilet} />
-        ))}
-      </div>
     </>
-  );
-};
-
-export default ToiletList;
+  )
