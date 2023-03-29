748f40be16b090caa824adad4833deb4bc9612d0
Chris Leech <chrisleech@monzo.com>
2023-03-29 11:18:44 +0100

    Updated tokenGenerator

--- backend/models/tokenGenerator.js
+++ backend/models/tokenGenerator.js
@@ -4,13 +4,15 @@ module.exports = (userId) =>
   jwt.sign(
     {
       userId,
-      // Set the JWT token to be issued 5 minutes ago
+      // Set the issued at time to the current time
       // iat = issued at
       iat: Math.floor(Date.now() / 1000),
 
-      // Set the JWT token to expire in 20 minutes
+      //! TODO: Set the JWT token to expire in 10 minutes in production
+
+      // Set the JWT token to expire in 60 minutes
       // exp = expires
-      exp: Math.floor(Date.now() / 1000) + 20 * 60,
+      exp: Math.floor(Date.now() / 1000) + 60 * 60,
     },
     // Wrap in a template literal to ensure the value is read as a string
     `${process.env.JWT_SECRET}`
