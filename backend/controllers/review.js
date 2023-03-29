d155e8a7e176c9cd10642660946d79d0dfb30dcb
terryhycheng <terryhycheng@gmail.com>
2023-03-29 14:11:04 +0100

    Added middleware & renamed

--- backend/controllers/review.js
+++ backend/controllers/review.js
@@ -1,4 +1,6 @@
 const Review = require("../models/review");
+const Toilet = require("../models/toilet");
+const generateToken = require("../models/tokenGenerator");
 
 const getAllReviews = async (req, res) => {
   try {
@@ -9,9 +11,11 @@ const getAllReviews = async (req, res) => {
       "username email"
     );
 
+    const token = await generateToken(req.userId);
+
     // If no comments are found, return an error
     if (!reviews) {
-      return res.status(404).json({ error: "No reviews added" });
+      return res.status(404).json({ error: "No reviews added", token });
     }
 
     const toiletReviews = reviews.map((review) => {
@@ -22,7 +26,7 @@ const getAllReviews = async (req, res) => {
         createdAt: review.createdAt,
       };
     });
-    return res.status(200).json({ toiletReviews });
+    return res.status(200).json({ toiletReviews, token });
   } catch (error) {
     return res.status(500).json({ error });
   }
@@ -33,17 +37,18 @@ const addReview = async (req, res) => {
     const { toiletId } = req.params;
 
     const toilet = await Toilet.find({ _id: toiletId });
+    const token = await generateToken(req.userId);
 
     if (!toilet) {
-      return res.status(404).json({ error: "Toilet not found" });
+      return res.status(404).json({ error: "Toilet not found", token });
     }
 
     const review = new Review(req.body);
     // userId should come from middleweare but not from the body
+    review.toiletId = toiletId;
     review.author = req.body.userId;
     await review.save();
-
-    return res.status(201).json({ message: "Successfully created" });
+    return res.status(201).json({ message: "Successfully created", token });
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
---
+++ backend/spec/controllers/reviews.spec.js
@@ -0,0 +1,109 @@
+require("../mongodb_helper");
+const request = require("supertest");
+const bcrypt = require("bcryptjs");
+const app = require("../../app");
+const jwt = require("jsonwebtoken");
+const Review = require("../../models/review");
+const Toilet = require("../../models/toilet");
+const User = require("../../models/user");
+
+let toilet;
+let user;
+let token;
+
+const generateBackdatedToken = (userId) =>
+  jwt.sign(
+    {
+      userId,
+      iat: Math.floor(Date.now() / 1000),
+
+      // Set the JWT token to expire in 10 minutes
+      exp: Math.floor(Date.now() / 1000) + 10 * 60,
+    },
+    // Wrap in a template literal to ensure the value is read as a string
+    `${process.env.JWT_SECRET}`
+  );
+
+describe("/toilet/review", () => {
+  beforeAll(async () => {
+    user = new User({
+      username: "amb",
+      email: "test@test.com",
+      password: bcrypt.hashSync("12345678", bcrypt.genSaltSync()),
+    });
+    await user.save();
+
+    toilet = new Toilet({
+      name: "Waterloo-1",
+      author: user.id,
+    });
+    await toilet.save();
+  });
+
+  beforeEach(async () => {
+    await Review.deleteMany({});
+  });
+
+  afterAll(async () => {
+    await Toilet.deleteMany({});
+    await User.deleteMany({});
+  });
+
+  describe("POST, when token is present", () => {
+    beforeEach(() => {
+      token = generateBackdatedToken(user.id);
+    });
+    test("responds with a 201", async () => {
+      const response = await request(app)
+        .post(`/toilets/${toilet.id}/review`)
+        .set("Authorization", `Bearer ${token}`)
+        .send({ clean: "3" });
+      expect(response.status).toEqual(201);
+    });
+    test("creates a new review", async () => {
+      await request(app)
+        .post(`/toilets/${toilet.id}/review`)
+        .set("Authorization", `Bearer ${token}`)
+        .send({ clean: "3" });
+      const review = await Review.find();
+      expect(review.length).toEqual(1);
+      expect(review[0].clean).toEqual(3);
+    });
+
+    test("returns a new token", async () => {
+      const response = await request(app)
+        .post(`/toilets/${toilet.id}/review`)
+        .set("Authorization", `Bearer ${token}`)
+        .send({ clean: "3" });
+      const newPayload = jwt.decode(
+        response.body.token,
+        process.env.JWT_SECRET
+      );
+      const originalPayload = jwt.decode(token, process.env.JWT_SECRET);
+      expect(newPayload.iat > originalPayload.iat).toEqual(true);
+    });
+  });
+
+  describe("POST, when token is missing", () => {
+    test("responds with a 401", async () => {
+      const response = await request(app)
+        .post(`/toilets/${toilet.id}/review`)
+        .send({ clean: "3" });
+      expect(response.status).toEqual(401);
+    });
+    test("a new reveiw is not created", async () => {
+      await request(app)
+        .post(`/toilets/${toilet.id}/review`)
+        .send({ clean: "3" });
+      const review = await Review.find();
+      expect(review.length).toEqual(0);
+    });
+
+    test("a new token is not returned", async () => {
+      const response = await request(app)
+        .post(`/toilets/${toilet.id}/review`)
+        .send({ clean: "3" });
+      expect(response.body.token).toEqual(undefined);
+    });
+  });
+});
