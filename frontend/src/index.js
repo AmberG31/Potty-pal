e601aa5f1ed1380c705ef051da878fc2a821472f
terryhycheng <terryhycheng@gmail.com>
2023-03-30 00:34:47 +0100

    Removed strict mode, wrap AuthContext inside router provider for redirect purpose

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,13 +8,11 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <AuthContextProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </AuthContextProvider>
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
