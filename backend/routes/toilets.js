0e1e2cac53e40f72e50d344096cb2751e231ba09
terryhycheng <terryhycheng@gmail.com>
2023-03-29 14:11:43 +0100

    Changed the import path

--- backend/routes/toilets.js
+++ backend/routes/toilets.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 const { getAllToilets, addNewToilet } = require("../controllers/toilets");
-const { getAllReviews, addReview } = require("../controllers/reviewController");
+const { getAllReviews, addReview } = require("../controllers/review");
 
 router.get("/", getAllToilets);
 router.post("/", addNewToilet);
