6896895dd0352dbbb824e2fae46bfcf81371b609
Chris Leech <chrisleech@monzo.com>
2023-03-29 16:25:08 +0100

    added timestamps to UserSchema

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,11 +2,14 @@ const mongoose = require("mongoose");
 
 // mongoose.set("useFindAndModify", false);
 
-const UserSchema = new mongoose.Schema({
-  username: { type: String, unique: true, required: true },
-  email: { type: String, unique: true, required: true },
-  password: { type: String, required: true },
-});
+const UserSchema = new mongoose.Schema(
+  {
+    username: { type: String, unique: true, required: true },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", UserSchema);
 
