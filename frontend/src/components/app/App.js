f389866b7bfae80584fca50fa8fa29fbe15e3bbf
Chris Leech <chrisleech@monzo.com>
2023-03-29 16:24:42 +0100

    update routes in App.js

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -1,12 +1,16 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, redirect } from "react-router-dom";
 
 import ToiletList from "../toiletList/ToiletList";
+import SignUpForm from "../signUpForm/SignUpForm";
+import LoginForm from "../loginForm/LoginForm";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/login" element={<LoginForm />} redirect={redirect} />
+        <Route path="/signup" element={<SignUpForm />} redirect={redirect} />
         <Route path="/" element={<ToiletList />} />
       </Routes>
     </BrowserRouter>
