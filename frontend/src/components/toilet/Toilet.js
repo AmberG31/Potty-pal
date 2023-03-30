0b05f7a4db4ab4ec5547a4ac138bd52588190848
Chris Leech <chrisleech@monzo.com>
2023-03-30 22:54:31 +0100

    creation of Toilet component

--- frontend/src/components/toilet/Toilet.js
+++ frontend/src/components/toilet/Toilet.js
@@ -4,8 +4,31 @@ const Toilet = ({ toilet }) => {
   return (
     <>
       {/* All of the toilet information should be rendered here */}
-      <div>
-        <p>{toilet.name}</p>
+      <div className="flex gap-x-4 rounded-md bg-gray-50 p-6 shadow-md">
+        <div id="toilet-photo">
+          <img
+            src="https://picsum.photos/500/500"
+            alt="toilet"
+            className="h-20 w-20 rounded-md"
+          />
+        </div>
+        <div id="toilet-info">
+          <p className="text-2xl font-bold capitalize">{toilet.name}</p>
+          <div className="flex gap-x-2">
+            <div className="flex gap-x-2">
+              <p className=" font-semibold">Accessible: </p>
+              <p className="">{toilet.accessible ? "Yes" : "No"}</p>
+            </div>
+            <div className="flex gap-x-2">
+              <p className="font-semibold">Baby Changing: </p>
+              <p className="">{toilet.babyChanging ? "Yes" : "No"}</p>
+            </div>
+          </div>
+          <div className="flex gap-x-2">
+            <p className="font-semibold">Price: </p>
+            <p className="">£ {toilet.price.$numberDecimal}</p>
+          </div>
+        </div>
       </div>
     </>
   );
