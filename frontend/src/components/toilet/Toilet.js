6225ff0dc414479ddbae3417a4ae18f500be34c4
Chris Leech <chrisleech@monzo.com>
2023-03-31 00:06:35 +0100

    updated Toilet frontend

--- frontend/src/components/toilet/Toilet.js
+++ frontend/src/components/toilet/Toilet.js
@@ -1,11 +1,17 @@
 import React from "react";
+import {
+  ArrowRightCircleIcon,
+  CheckCircleIcon,
+  XCircleIcon,
+} from "@heroicons/react/24/solid";
+import { Link } from "react-router-dom";
 
 const Toilet = ({ toilet }) => {
   return (
     <>
       {/* All of the toilet information should be rendered here */}
       <div className="flex gap-x-4 rounded-md bg-gray-50 p-6 shadow-md">
-        <div id="toilet-photo">
+        <div id="toilet-photo" className="flex items-center">
           <img
             src="https://picsum.photos/500/500"
             alt="toilet"
@@ -13,21 +19,36 @@ const Toilet = ({ toilet }) => {
           />
         </div>
         <div id="toilet-info">
-          <p className="text-2xl font-bold capitalize">{toilet.name}</p>
+          <p className="gap-y-2 text-2xl font-bold capitalize">{toilet.name}</p>
           <div className="flex gap-x-2">
-            <div className="flex gap-x-2">
-              <p className=" font-semibold">Accessible: </p>
-              <p className="">{toilet.accessible ? "Yes" : "No"}</p>
-            </div>
-            <div className="flex gap-x-2">
-              <p className="font-semibold">Baby Changing: </p>
-              <p className="">{toilet.babyChanging ? "Yes" : "No"}</p>
-            </div>
+            <p className=" font-semibold">Accessible</p>
+            <p className="">
+              {toilet.accessible ? (
+                <CheckCircleIcon className="h-6 w-6 text-green-500" />
+              ) : (
+                <XCircleIcon className="h-6 w-6 text-red-500" />
+              )}
+            </p>
+          </div>
+          <div className="flex gap-x-2">
+            <p className="font-semibold">Baby Changing</p>
+            <p className="">
+              {toilet.babyChanging ? (
+                <CheckCircleIcon className="h-6 w-6 text-green-500" />
+              ) : (
+                <XCircleIcon className="h-6 w-6 text-red-500" />
+              )}
+            </p>
           </div>
           <div className="flex gap-x-2">
             <p className="font-semibold">Price: </p>
             <p className="">£ {toilet.price.$numberDecimal}</p>
           </div>
+        </div>
+        <div className="ml-auto flex items-center" id="more-info-button">
+          <Link to={`/toilets/${toilet._id}`} className="cursor-pointer">
+            <ArrowRightCircleIcon className="h-14 w-14 justify-center text-blue-500 hover:text-blue-600" />
+          </Link>
         </div>
       </div>
     </>
