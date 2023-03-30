153604a3aa37d2224dad030bf7493acd119a91e8
Chris Leech <chrisleech@monzo.com>
2023-03-30 22:54:48 +0100

    update styling of ToiletList

--- frontend/src/components/toiletList/ToiletList.js
+++ frontend/src/components/toiletList/ToiletList.js
@@ -3,8 +3,8 @@ import Toilet from "../toilet/Toilet";
 
 const ToiletList = ({ toilets }) => {
   return (
-    <div className="flex flex-col gap-2">
-      <div id="toilet-list">
+    <div className="">
+      <div id="toilet-list" className="flex flex-col gap-y-6">
         {toilets.map((toilet) => (
           <Toilet key={toilet._id} toilet={toilet} />
         ))}
