3f3c92a0e4348b23e8596be95fcd511dd27c398a
Chris Leech <chrisleech@monzo.com>
2023-03-31 14:20:26 +0100

    added import

--- frontend/cypress/support/component.js
+++ frontend/cypress/support/component.js
@@ -14,14 +14,15 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import './commands';
+import 'cypress-real-events';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
-import { mount } from 'cypress/react18'
+import { mount } from 'cypress/react18';
 
-Cypress.Commands.add('mount', mount)
+Cypress.Commands.add('mount', mount);
 
 // Example use:
 // cy.mount(<MyComponent />)
