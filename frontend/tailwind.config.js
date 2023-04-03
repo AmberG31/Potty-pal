2cf3fd364440bff9b3f068bf18af874ec1142d23
terryhycheng <terryhycheng@gmail.com>
2023-04-03 12:53:05 +0100

    Added default colors and btn style

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#F7942C',
+      },
+    },
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: ['@tailwindcss/forms'],
 };
