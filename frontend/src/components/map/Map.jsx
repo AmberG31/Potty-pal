944274b6e7c79e20abcd1808c52d868368f44480
terryhycheng <terryhycheng@gmail.com>
2023-04-03 12:51:01 +0100

    Added content and styles for popup
    Co-authored-by: Amber <AmberG31@users.noreply.github.com>

--- frontend/src/components/map/Map.jsx
+++ frontend/src/components/map/Map.jsx
@@ -10,9 +10,33 @@ const toilet = new Icon({
 
 function PopupContainer() {
   return (
-    <div className="h-52 w-52">
-      <h2 className="text-xl font-bold">Toilet A</h2>
-      <p>Content</p>
+    <div className="h-80 w-80 overflow-auto">
+      <h2 className="text-xl font-bold">Toilet name</h2>
+      <div className="text-gray-500">
+        153 London High Street, Waterloo, W3 5PH
+      </div>
+      <div className="font-bold text-orange-600">3.5 *** (ratings)</div>
+      <div className="text-xs text-gray-500"> based on 21 reviews</div>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwGdczG6yAHDyI-DJW0TmjHK0Id8CZfouh2FXAnQcuDG9oC5op0YYcIJhYYKj_ycbB7AQ&usqp=CAU"
+        alt=""
+        className="h-50 w-50 object-cover"
+      />
+      <div>
+        <h3 className="text-m font-bold"> Facilities </h3>
+        <p className="text-s" style={{ columnCount: 2 }}>
+          Unisex Accesible Baby-changing Sanitary dressings Hot & cold water
+        </p>
+        <div>
+          <h3 className="text-m font-bold"> Price </h3>
+          <p className="text-s divide-y divide-slate-100">Free (add price)</p>
+        </div>
+      </div>
+      <div className="text-right">
+        <button type="button" className="btn w-20">
+          Reviews
+        </button>
+      </div>
     </div>
   );
 }
