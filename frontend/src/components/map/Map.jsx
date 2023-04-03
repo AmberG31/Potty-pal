6a52676f79e9cf2f57ae3ba7bf9ebc3a95bec195
terryhycheng <terryhycheng@gmail.com>
2023-04-03 15:28:23 +0100

    Added star icons, tick icons. Stars filling based on the rating
    Co-authored-by: Amber <118190812+AmberG31@users.noreply.github.com>

--- frontend/src/components/map/Map.jsx
+++ frontend/src/components/map/Map.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { StarIcon, CheckCircleIcon } from '@heroicons/react/24/solid';
 
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import { Icon } from 'leaflet';
@@ -9,13 +10,44 @@ const toilet = new Icon({
 });
 
 function PopupContainer() {
+  const rating = 4;
+
   return (
     <div className="h-80 w-80 overflow-auto">
-      <h2 className="text-xl font-bold">Toilet name</h2>
-      <div className="text-gray-500">
-        153 London High Street, Waterloo, W3 5PH
+      <h2 className="text-xl font-bold">My beautiful toilet</h2>
+      <p className="text-gray-500">153 London High Street, Waterloo, W3 5PH</p>
+      <div className="font-bold text-orange-600">
+        <div className="flex items-center">
+          <span className="mr-2" style={{ display: 'inline-block' }}>
+            4
+          </span>
+          <StarIcon
+            className={`w-4 ${
+              rating >= 1 ? 'text-orange-600' : 'text-gray-400'
+            }`}
+          />
+          <StarIcon
+            className={`w-4 ${
+              rating >= 2 ? 'text-orange-600' : 'text-gray-400'
+            }`}
+          />
+          <StarIcon
+            className={`w-4 ${
+              rating >= 3 ? 'text-orange-600' : 'text-gray-400'
+            }`}
+          />
+          <StarIcon
+            className={`w-4 ${
+              rating >= 4 ? 'text-orange-600' : 'text-gray-400'
+            }`}
+          />
+          <StarIcon
+            className={`w-4 ${
+              rating >= 5 ? 'text-orange-600' : 'text-gray-400'
+            }`}
+          />
+        </div>
       </div>
-      <div className="font-bold text-orange-600">3.5 *** (ratings)</div>
       <div className="text-xs text-gray-500"> based on 21 reviews</div>
       <img
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwGdczG6yAHDyI-DJW0TmjHK0Id8CZfouh2FXAnQcuDG9oC5op0YYcIJhYYKj_ycbB7AQ&usqp=CAU"
@@ -24,12 +56,35 @@ function PopupContainer() {
       />
       <div>
         <h3 className="text-m font-bold"> Facilities </h3>
-        <p className="text-s" style={{ columnCount: 2 }}>
-          Unisex Accesible Baby-changing Sanitary dressings Hot & cold water
+        <p className="text-s grid grid-cols-2 gap-2">
+          <div className="flex items-center">
+            <CheckCircleIcon className="w-3 text-green-400" />
+            <span className="break-all">Unisex </span>
+          </div>
+          <div className="flex items-center">
+            <CheckCircleIcon className="w-3 text-green-400" />
+            <span className="break-all">Accesible </span>
+          </div>
+          <div className="flex items-center">
+            <CheckCircleIcon className="w-3 text-gray-400" />
+            <span className="break-all">Baby-changing </span>
+          </div>
+          <div className="flex items-center">
+            <CheckCircleIcon className="w-3 text-gray-400" />
+            <span className="break-all">Sanitary dressings </span>
+          </div>
+          <div className="flex items-center">
+            <CheckCircleIcon className="w-3 text-green-400" />
+            <span className="break-all">Hot &amp; cold water </span>
+          </div>
+          <div className="flex items-center">
+            <CheckCircleIcon className="w-3 text-gray-400" />
+            <span className="break-all">Toilet paper </span>
+          </div>
         </p>
         <div>
           <h3 className="text-m font-bold"> Price </h3>
-          <p className="text-s divide-y divide-slate-100">Free (add price)</p>
+          <p className="text-s divide-y divide-slate-100">Free</p>
         </div>
       </div>
       <div className="text-right">
