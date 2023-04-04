b343ea3890cb466092db53168ebfa0f6471c035f
terryhycheng <terryhycheng@gmail.com>
2023-04-04 11:41:06 +0100

    Added `RatingStars.jsx`
    Co-authored-by: Amber <AmberG31@users.noreply.github.com>

---
+++ frontend/src/components/ratingStars/RatingStars.jsx
@@ -0,0 +1,22 @@
+import { StarIcon } from '@heroicons/react/24/solid';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+function RatingStars({ rating }) {
+  return (
+    <div className="flex text-primary">
+      {Array.from({ length: rating }, () => (
+        <StarIcon className="w-5" />
+      ))}
+      {Array.from({ length: 5 - Math.floor(rating) }, () => (
+        <StarIcon className="w-5 text-gray-100" />
+      ))}
+    </div>
+  );
+}
+
+RatingStars.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
+
+export default RatingStars;
