abea0a1f356358cb136f7734a2f6f2adc2c2cd39
Amber <gynteee@yahoo.co.uk>
2023-04-04 16:30:15 +0100

    Updated the layouts
    Co-authored-by: Terry Cheng <terryhycheng@users.noreply.github.com>

--- frontend/src/components/toiletInfo/ToiletInfo.jsx
+++ frontend/src/components/toiletInfo/ToiletInfo.jsx
@@ -1,47 +1,81 @@
-import React from 'react';
+/* eslint-disable react/jsx-one-expression-per-line */
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { CheckCircleIcon } from '@heroicons/react/24/outline';
+
+import { MapContainer, Marker, TileLayer } from 'react-leaflet';
+import { Icon } from 'leaflet';
 import RatingStars from '../ratingStars/RatingStars';
 
-function ToiletInfo({ toiletData }) {
-  const rating = 4.23;
+function ToiletInfo({ toiletData, ratings }) {
   const { address, city, postcode } = toiletData.address;
-  const { babyChanging, accessible } = toiletData;
+  const { babyChanging, accessible, reviews } = toiletData;
+  const { cleanliness } = ratings;
+
+  const [center] = useState([51.505, -0.09]);
+  const toilet = new Icon({
+    iconUrl: '/toilet.svg',
+    iconSize: [60, 60],
+  });
+
+  const imageUrl =
+    toiletData.photos.length === 0
+      ? '/no-image-uploaded.jpg'
+      : toiletData.photos[0];
+
   return (
     <div>
       <div className="my-10">
         <h1 className="text-3xl font-bold">{toiletData.name}</h1>
         <p className="mt-2 text-lg text-gray-400">{`${address}, ${city}, ${postcode}`}</p>
-        <div className="mt-2 flex items-center gap-3 text-lg font-semibold text-primary">
-          {rating}
-          <RatingStars rating={rating} />
-          <p className="text-base font-light text-gray-400">
-            based on 21 reviews
-          </p>
-        </div>
+        {reviews.length === 0 ? (
+          <div className="mt-2 flex items-center gap-3 text-lg font-semibold">
+            No reviews
+          </div>
+        ) : (
+          <div className="mt-2 flex items-center gap-3 text-lg font-semibold text-primary">
+            {cleanliness.toFixed(1)}
+            <RatingStars rating={cleanliness} />
+            <p className="text-base font-light text-gray-400">
+              based on {toiletData.reviews.length} reviews
+            </p>
+          </div>
+        )}
       </div>
-      <div className="flex gap-8">
+      <div className="flex items-stretch gap-8">
         {/* Photos */}
-        <div className="flex-1">
+        <div className="flex flex-1 flex-col">
           <h2 className="text-2xl font-bold">Photos</h2>
-          <div className="mt-2">
-            <img src={toiletData.photos[1]} alt="" />
+          <div className="mt-2 h-full rounded-lg border">
+            <img src={imageUrl} alt="" />
           </div>
         </div>
-        <div className="flex w-[40%] flex-col gap-8">
+        <div className="flex h-full w-[40%] flex-col gap-8">
           {/* Map */}
-          <div className="flex-1">
+          <div className="">
             <h2 className="text-2xl font-bold">Map</h2>
-            <div className="mt-2">123</div>
+            <MapContainer
+              center={center}
+              zoom={15}
+              scrollWheelZoom
+              className="mt-2 h-52 w-full"
+            >
+              <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <Marker position={center} icon={toilet} />
+            </MapContainer>
           </div>
           {/* Overall Ratings */}
-          <div>
+          <div className="flex-1">
             <h2 className="text-2xl font-bold">Overall Ratings</h2>
             <div className="mt-4 flex flex-col gap-3 rounded-lg border p-6">
-              <Rating name="Cleanliness" rating={3.21} />
-              <Rating name="Cleanliness" rating={4.13} />
-              <Rating name="Cleanliness" rating={1.21} />
-              <Rating name="Cleanliness" rating={2.21} />
+              {reviews.length === 0 ? (
+                'No reviews'
+              ) : (
+                <Rating name="Cleanliness" rating={cleanliness} />
+              )}
             </div>
           </div>
         </div>
@@ -71,7 +105,7 @@ function Rating({ name, rating }) {
     <div className="flex justify-between">
       {name}
       <div className="flex gap-3">
-        <p className="text-primary">{rating}</p>
+        <p className="text-primary">{rating.toFixed(1)}</p>
         <RatingStars rating={rating} />
       </div>
     </div>
@@ -91,7 +125,7 @@ function FacilityIcon({ name, isChecked }) {
 
 Rating.propTypes = {
   name: PropTypes.string.isRequired,
-  rating: PropTypes.bool.isRequired,
+  rating: PropTypes.number.isRequired,
 };
 
 FacilityIcon.propTypes = {
@@ -117,7 +151,9 @@ ToiletInfo.propTypes = {
     city: PropTypes.string,
     postcode: PropTypes.string,
     photos: PropTypes.arrayOf(PropTypes.string),
+    reviews: PropTypes.arrayOf(PropTypes.shape({})),
   }).isRequired,
+  ratings: PropTypes.number.isRequired,
 };
 
 export default ToiletInfo;
