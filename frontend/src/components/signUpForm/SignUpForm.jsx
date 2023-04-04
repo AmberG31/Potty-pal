75e25a884b4f7b96a10506a9f73adae24fc3e919
terryhycheng <terryhycheng@gmail.com>
2023-04-04 21:19:56 +0100

    Merge branch 'main' into map-page-layout

--- frontend/src/components/signUpForm/SignUpForm.jsx
+++ frontend/src/components/signUpForm/SignUpForm.jsx
@@ -32,47 +32,73 @@ function SignUpForm() {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">
-          Email
-          <input
-            type="email"
-            name="email"
-            id="email"
-            ref={emailRef}
-            autoComplete="email"
-            required
-          />
-        </label>
-        <label htmlFor="username">
-          Username
-          <input
-            type="text"
-            id="username"
-            ref={usernameRef}
-            autoComplete="username"
-            required
-          />
-        </label>
-        <label htmlFor="password">
-          Password
-          <input
-            type="password"
-            id="password"
-            ref={passwordRef}
-            autoComplete="current-password"
-            required
-          />
-        </label>
-        <button
-          className="w-full rounded-lg bg-blue-600 p-2 text-sm font-bold text-white transition-all hover:bg-blue-500 disabled:bg-gray-500"
-          type="submit"
-          id="submit-signup"
-        >
-          Sign Up
-        </button>
-      </form>
+    <div className="flex items-center justify-center bg-gray-50 px-4 py-10 ">
+      <div className=" w-full max-w-md rounded-lg bg-white shadow-lg">
+        <div className="mt-4 py-4 text-center">
+          <img src="/mini-logo.svg" alt="logo" className="mx-auto h-16 w-16" />
+          <h2 className="mt-2 text-2xl font-bold">Signup</h2>
+        </div>
+        <div className="px-8 py-4">
+          <form onSubmit={handleSubmit}>
+            <div className="mb-4">
+              <label
+                htmlFor="email"
+                className="mb-2 block font-bold text-gray-700"
+              >
+                Email
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  ref={emailRef}
+                  autoComplete="email"
+                  required
+                  className="form-input w-full rounded-lg border border-gray-400 px-4 py-2 focus:outline-none"
+                />
+              </label>
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="username"
+                className="mb-2 block font-bold text-gray-700"
+              >
+                Username
+                <input
+                  type="text"
+                  id="username"
+                  ref={usernameRef}
+                  autoComplete="username"
+                  required
+                  className="form-input w-full rounded-lg border border-gray-400 px-4 py-2 focus:outline-none"
+                />
+              </label>
+            </div>
+            <div className="mb-6">
+              <label
+                htmlFor="password"
+                className="mb-2 block font-bold text-gray-700"
+              >
+                Password
+                <input
+                  type="password"
+                  id="password"
+                  ref={passwordRef}
+                  autoComplete="current-password"
+                  required
+                  className="form-input w-full rounded-lg border border-gray-400 px-4 py-2 focus:outline-none"
+                />
+              </label>
+            </div>
+            <button
+              className="mb-2 w-full rounded-lg bg-primary px-4 py-2 text-sm font-bold text-white transition-all hover:bg-orange-500 disabled:bg-gray-500"
+              type="submit"
+              id="submit-signup"
+            >
+              Sign Up
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,41 +1,28 @@
 import axios from 'axios';
-import React, { useContext, useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
-
-import { AuthContext } from '../context/AuthContext';
 
 import ToiletList from '../components/toiletList/ToiletList';
 import { ApiUrlContext } from '../context/ApiUrlContext';
 
 function Home() {
   const [toilets, setToilets] = useState([]);
-  const { token, tokenHandler } = useContext(AuthContext);
   const navigate = useNavigate();
   const { url } = useContext(ApiUrlContext);
 
   const getToilets = useCallback(async () => {
-    if (token === undefined || token === null) {
-      navigate('/login');
-      return;
-    }
-
     try {
-      const response = await axios.get(`${url}/toilets`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
+      const response = await axios.get(`${url}/toilets`);
 
       if (response.status !== 200) {
         throw new Error('Failed to fetch toilets');
       } else {
-        tokenHandler(response.data.token);
         setToilets(response.data.toilets);
       }
     } catch (error) {
       throw new Error(`Error: ${error.message}`);
     }
-  }, [token, tokenHandler, navigate]);
+  }, [navigate]);
 
   useEffect(() => {
     getToilets();
