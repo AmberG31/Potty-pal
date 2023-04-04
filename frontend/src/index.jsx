51fbc60866ca8f4e241ce2f3f59c98f3569fa14f
terryhycheng <terryhycheng@gmail.com>
2023-04-04 11:34:32 +0100

    Changed to `HashRouter` for deployment

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import App from './components/app/App';
 import AuthContextProvider from './context/AuthContext';
 import ModalContextProvider from './context/ModalContext';
+import ApiUrlContextProvider from './context/ApiUrlContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <BrowserRouter>
-    <AuthContextProvider>
-      <ModalContextProvider>
-        <App />
-      </ModalContextProvider>
-    </AuthContextProvider>
-  </BrowserRouter>
+  <Router>
+    <ApiUrlContextProvider>
+      <AuthContextProvider>
+        <ModalContextProvider>
+          <App />
+        </ModalContextProvider>
+      </AuthContextProvider>
+    </ApiUrlContextProvider>
+  </Router>
 );
