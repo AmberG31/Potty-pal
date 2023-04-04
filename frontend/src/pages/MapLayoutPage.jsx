5e9269afe97338ceaff7c0a9f80ab82bd8397af7
terryhycheng <terryhycheng@gmail.com>
2023-04-04 11:43:19 +0100

    Fixed layout and navbar display issues

--- frontend/src/pages/MapLayoutPage.jsx
+++ frontend/src/pages/MapLayoutPage.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import Map from '../components/map/Map';
-import Navbar from '../components/navbar/Navbar';
 import Sidebar from '../components/sidebar/Sidebar';
 
 function MapLayoutPage() {
   return (
-    <div className="flex h-[100vh] flex-col">
-      <Navbar />
-      <div className="flex flex-1">
-        <Sidebar />
-        <div className="flex-1">
-          <Map />
-        </div>
+    <div className="flex h-full flex-1">
+      <Sidebar />
+      <div className="flex-1">
+        <Map />
       </div>
     </div>
   );
