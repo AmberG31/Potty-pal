89296ad6dfd593e9e6ec94f5eafe898ce5acf5a9
Chris Leech <chrisleech@monzo.com>
2023-04-04 14:15:10 +0100

    Removed uneeded h1

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -2,12 +2,7 @@ import React from 'react';
 import LoginForm from '../components/loginForm/LoginForm';
 
 function LoginPage() {
-  return (
-    <>
-      <h1>Log in</h1>
-      <LoginForm />
-    </>
-  );
+  return <LoginForm />;
 }
 
 export default LoginPage;
