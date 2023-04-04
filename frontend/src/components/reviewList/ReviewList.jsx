ce4860ea78b51d3d541b5a95c9a2f6b3be085c08
terryhycheng <terryhycheng@gmail.com>
2023-04-04 21:24:36 +0100

    Made `add review` button hidden when no `token` is found

--- frontend/src/components/reviewList/ReviewList.jsx
+++ frontend/src/components/reviewList/ReviewList.jsx
@@ -1,8 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Review, { reviewPropTypes } from '../review/Review';
+import { AuthContext } from '../../context/AuthContext';
 
 function ReviewList({ reviews, setIsModal, setRatings }) {
+  const { token } = useContext(AuthContext);
+
   const calculateRating = () => {
     if (reviews.length === 0) {
       return;
@@ -25,13 +28,15 @@ function ReviewList({ reviews, setIsModal, setRatings }) {
     <>
       <div className="my-6 flex items-center justify-between">
         <h2 className="text-2xl font-bold">Reviews</h2>
-        <button
-          type="button"
-          className="btn px-10"
-          onClick={() => setIsModal(true)}
-        >
-          Add review
-        </button>
+        {token && (
+          <button
+            type="button"
+            className="btn px-10"
+            onClick={() => setIsModal(true)}
+          >
+            Add review
+          </button>
+        )}
       </div>
       <div className="flex flex-col gap-4">
         {!reviews.length && <p>No reviews for this toilet</p>}
